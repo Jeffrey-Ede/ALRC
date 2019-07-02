5269ca3236feb5bfaf860b280f38700918c5acdd
Jeffrey Ede <jeffreyede1@outlook.com>
2019-07-02 12:41:27 +0100

    Better variable names

--- alrc.py
+++ alrc.py
@@ -11,17 +11,62 @@ if the first raw moments of the momentum are on the scale of the losses.
 
 import tensorflow as tf
 
-alrc_counter = 0
-def alrc(loss, num_stddev=3, decay=0.999, mu1_start=25, mu2_start=30**2):
+def auto_name(name):
+    """Append number to variable name to make it unique.
+    
+    Inputs:
+        name: Start of variable name.
 
-    global alrc_counter; alrc_counter += 1
+    Returns:
+        Full variable name with number afterwards to make it unique.
+    """
+
+    scope = tf.contrib.framework.get_name_scope()
+    vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=scope)
+
+    names = [v.name for v in vars]
+    
+    #Increment variable number until unused name is found
+    for i in itertools.count():
+        short_name = name + "_" + str(i)
+        sep = "/" if scope != "" else ""
+        full_name = scope + sep + short_name
+        if not full_name in [n[:len(full_name)] for n in names]:
+            return short_name
+
+
+def alrc(
+    loss, 
+    num_stddev=3, 
+    decay=0.999, 
+    mu1_start=25, 
+    mu2_start=30**2, 
+    in_place_updates=False
+    ):
+    """Adaptive learning rate clipping (ALRC) of outlier losses.
+    
+    Inputs:
+        loss: Loss function to limit outlier losses of.
+        num_stddev: Number of standard deviation above loss mean to limit it
+        to.
+        decay: Decay rate for exponential moving averages used to tract first
+        two raw moments of the loss.
+        mu1_start: Initial estimate for the first raw moment of the loss.
+        mu2_start: Initial estimate for the second raw moment of the loss.
+        in_place_updates: If True, add control dependencies for moment tracking
+        to tf.GraphKeys.UPDATE_OPS. This allows the control dependencies to be
+        executed in parallel with other dependencies later.
+
+    Return:
+        Loss function with control dependencies for ALRC.
+    """
 
     #Varables to track first two raw moments of the loss
     mu = tf.get_variable(
-        f"mu-{alrc_counter}", 
+        auto_name("mu1"), 
         initializer=tf.constant(mu1_start, dtype=tf.float32))
     mu2 = tf.get_variable(
-        f"mu2-{alrc_counter}", 
+        auto_name("mu2"), 
         initializer=tf.constant(mu2_start, dtype=tf.float32))
 
     #Use capped loss for moment updates to limit the effect of extreme losses on the threshold
@@ -30,8 +75,15 @@ def alrc(loss, num_stddev=3, decay=0.999, mu1_start=25, mu2_start=30**2):
                    loss, 
                    loss/tf.stop_gradient(loss/(mu+num_stddev*sigma)))
 
-    #Update moments
-    with tf.control_dependencies([mu.assign(decay*mu+(1-decay)*loss), mu2.assign(decay*mu2+(1-decay)*loss**2)]):
-        loss = tf.identity(loss)
-        
-    return loss
+    #Update moment moving averages
+    update_ops = [mu.assign(decay*mu+(1-decay)*loss), 
+                  mu2.assign(decay*mu2+(1-decay)*loss**2)]
+    if in_place_updates:
+        with tf.control_dependencies(update_ops):
+            loss = tf.identity(loss)
+            return loss
+    else:
+        for update_op in update_ops:
+            #Execute control dependency in parallel with other update ops
+            tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, update_op)
+        return loss
